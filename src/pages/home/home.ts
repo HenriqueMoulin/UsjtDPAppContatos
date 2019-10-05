1204f4ec8f708691908d462847c046eb1035f388
arqdsis <arqdsis@MOOLIi0327.animaedu.intranet>
2019-10-05 16:20:39 -0300

    Aula02 - Contato - DP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  contatos = [
+    {
+      nome: 'Henrique',
+      telefone: "123456789r",
+      email: "henrique@gmail.com" 
+    },
+    {
+      nome: 'Henrique1',
+      telefone: "123456789r",
+      email: "henrique1@gmail.com"
+    },
+    {
+      nome: 'Henrique2',
+      telefone: "123456789r",
+      email: "henrique2@gmail.com"
+    }
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  onItemClick (contato){
+    //console.log ('item-click', contato);
+    this.navCtrl.push ('DetailPage', {contatoParam: contato });
+   }
 
 }
